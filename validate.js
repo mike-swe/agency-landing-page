8a60d8ca3b697d31075900d7e080b4de31cf6e29
Mike Chambers <83576928+mainfunktion@users.noreply.github.com>
2022-01-29 07:33:00 -0800

    initial commit

---
+++ validate.js
@@ -0,0 +1,36 @@
+let loginForm = () => {
+    const name = document.getElementById("name");
+
+    const email = document.getElementById("email");
+
+    const message = document.getElementById("message");
+
+    const button = document.getElementById("cta-2");
+
+    if(name.value.trim() ==="" || email.value.trim() ==="" && message.value.trim() ==="") {
+        alert("Input required on required field!!");
+
+        name.placeholder = "Please enter your name";
+
+        email.placeholder = "Please enter your email";
+
+        name.style.border = "2px solid red";
+
+        email.style.border = "2px solid red";
+
+        message.placeholder = "Say something here";
+
+        name.addEventListener("click", e => {
+
+            name.style.border = "2px solid transparent";
+
+        });
+
+        return false
+
+    } else {
+
+        true
+
+    }
+}
